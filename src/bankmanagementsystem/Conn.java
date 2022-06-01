57fa25805a5356d306509dabfa5cf890015a1b87
Sudeep Ghosh <sudeepghosh1411@gmail.com>
2022-06-01 11:28:35 +0530

    Commit

---
+++ src/bankmanagementsystem/Conn.java
@@ -0,0 +1,18 @@
+
+package bankmanagementsystem;
+import java.sql.*;
+public class Conn {
+    Connection c;
+    Statement s;
+    
+    public Conn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc.mysql:///ATMSystem","root","");
+            s=c.createStatement();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
